36dba2fb6ac9aa235fd7f5026c57e9786cd2c481
Clayman <Clayman@DESKTOP-DM8E77U>
2023-05-22 12:09:44 +0300

    List2 start

---
+++ DataContainers/List2/main.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+
+class List
+{
+	class Element
+	{
+		int Data;	//значение элемента
+		Element* pNext;	//указатель на следующий элемент
+		Element* pPrev;	//указатель на предыдущий элемент
+	public:
+		Element(int Data, Element* pNext = nullptr, Element* pPrev = nullptr) 
+			:Data(Data), pNext(pNext), pPrev(pPrev)
+		{
+			cout << "EConstructor:\t" << this << endl;
+		}
+		~Element()
+		{
+			cout << "EDestructor:\t" << this << endl;
+		}
+		friend class List;
+	};
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+}
