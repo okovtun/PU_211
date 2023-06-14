8a2cc1d60b5746096b7ec8c10225370745b4b316
Clayman <Clayman@DESKTOP-DM8E77U>
2023-06-14 15:18:30 +0300

    Vector

--- STL/STL/main.cpp
+++ STL/STL/main.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<array>
+#include<stack>
+#include<vector>
 using std::cin;
 using std::cout;
 using std::endl;
@@ -7,7 +9,11 @@ using std::endl;
 #define tab "\t"
 #define delimiter "\n-----------------------------------------\n"
 
-#define STL_ARRAY
+template<typename T>void vector_properties(const std::vector<T>& vec);
+
+//#define STL_ARRAY
+//#define STL_STACK
+#define STL_VECTOR
 
 void main()
 {
@@ -25,4 +31,56 @@ void main()
 	cout << endl;
 #endif // STL_ARRAY
 
+#ifdef STL_STACK
+	std::stack<int> stack;
+	stack.push(3);
+	stack.push(5);
+	stack.push(8);
+	stack.push(13);
+	stack.push(21);
+	while (!stack.empty())
+	{
+		cout << stack.top() << endl;
+		stack.pop();
+	}
+#endif // STL_STACK
+
+#ifdef STL_VECTOR
+	//vector - это контейнер, который хранит данные в виде динамиечского массива;
+	//push_back();
+	//push_front();
+	//insert();
+	//erase();
+	std::vector<int> vec = { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+	for (int i = 0; i < vec.size(); i++)
+	{
+		cout << vec.operator[](i) << tab;
+	}
+	cout << endl;
+	vector_properties(vec);
+	vec.push_back(55);
+	vector_properties(vec);
+	vec.reserve(25);	//Резервирует дополнительную вместительность
+	//vec.shrink_to_fit();//Урезает вместительность до фактического размера вектора
+	vec.resize(8);
+	for (std::vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		cout << *it << tab;
+	}
+	cout << endl;
+	vector_properties(vec);
+	
+	cout << delimiter << endl;
+	cout << std::vector<int>().max_size() << endl;
+	cout << std::vector<double>().max_size() << endl;
+#endif // STL_VECTOR
+
+
+}
+
+template<typename T>void vector_properties(const std::vector<T>& vec)
+{
+	cout << "Size:\t " << vec.size() << endl;
+	cout << "Max size:" << vec.max_size() << endl;
+	cout << "Capacity:" << vec.capacity() << endl;
 }
